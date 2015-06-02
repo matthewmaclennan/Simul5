62b849578453f6e02b905cf9521f904773383d81
Matthew S. MacLennan <msmaclennan@gmail.com>
2015-06-02 15:33:15 -0700

    Update handling.R

--- handling.R
+++ handling.R
@@ -1,1 +1,12 @@
 #R script for handling the *.csv outputs from Simul5 Complex.
+#import sim files from a run
+ files<-list.files("C:\\Users\\ChenLab\\Documents\\Simul 5 Complex - Release 2011\\Export Profiles\\start4\\",
+ pattern="sim.*.csv",full.name=T)
+ filenames<-list.files("C:\\Users\\ChenLab\\Documents\\Simul 5 Complex - Release 2011\\Export Profiles\\start4\\",
+ pattern="sim.*.csv",full.name=F)
+#Replace spaces in the filenames with underscores so that assign() will work. The spaces are considered illegal name characters
+ filenames<-gsub(" ","_",filenames)
+  for(i in 1:length(files)){
+    assign(filename[i],read.csv(files[i]))
+  }
+  
